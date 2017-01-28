545ad3ef716494d4197c39385b6226da72d9b437
Piotr Torczynski <piotr.torczynski@netguru.pl>
2017-01-28 22:05:59 +0100

    Project update

---
+++ HDAugmentedReality/HDAugmentedReality.h
@@ -0,0 +1,19 @@
+//
+//  HDAugmentedReality.h
+//  HDAugmentedReality
+//
+//  Created by Piotr Torczynski on 28/01/2017.
+//  Copyright © 2017 SmartApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDAugmentedReality.
+FOUNDATION_EXPORT double HDAugmentedRealityVersionNumber;
+
+//! Project version string for HDAugmentedReality.
+FOUNDATION_EXPORT const unsigned char HDAugmentedRealityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDAugmentedReality/PublicHeader.h>
+
+
